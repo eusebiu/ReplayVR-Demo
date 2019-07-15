b81a7fa11b047ac3a937b07619b92e62764ed5e7
Eusebiu Marcu <eusebiu.marcu2@ucb.com>
2019-07-15 22:28:41 +0300

    first commit

---
+++ Assets/Scripts/Tests/When_Typing_Text_Test.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using ReplayVR.Core;
+using ReplayVR.Models;
+#endif
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+public class When_Typing_Text_Test : AssertBehaviour
+{
+	new void Awake()
+	{
+		base.Awake();
+	}
+	new void Start()
+    {
+		base.Start();  
+    }
+
+	// Update is called once per frame
+	new void Update()
+    {
+		base.Update();
+	}
+
+	protected override void HandleCurrentState(StateArgs e)
+	{
+		if (e.States[0] is ActionState actionState && actionState.Action == action && e.StateIndex == Index)
+		{
+			Assert(
+				GameObject.Find("ExampleWorldObjects/WorldKeyboard/Input").GetComponent<InputField>().text == "REPLAYVR",
+				"Test failed! :(", "Test success! :)");
+		}
+	}
+}
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ReplayVR-Demo
-Demo project for Replay for Unity (VR) - A (VR) testing framework for Unity. Currently is focued on VR.
+Demo project for Replay for Unity (VR) - A (VR) testing framework for Unity. Currently is focused on VR.
+
+Supports:
+- VRTK 3.2.1
+- SteamVR 1.2.3
+
+# Instructions
+The project demo comes with both VRTK and SteamVR (versions supported above).
+All you need to do is to get the ReplayVR asset from Unity Asset Store and put it inside Plugins folder and Include only Editor platform.
